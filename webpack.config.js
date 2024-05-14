5ed08b9460dba77f97288eec80cd5e9509021e34
b21phiro <philip.rosenqvist@outlook.com>
2024-05-14 14:03:33 +0200

    Init

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    mode: 'development',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            title: "Coffee Shop",
+            publicPath: '/'
+        }),
+        new FaviconsWebpackPlugin({
+            logo: './src/assets/images/logo.png',
+            publicPath: '/',
+            prefix: '/'
+        })
+    ],
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
